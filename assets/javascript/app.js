bab3ca236c98b0fc105c18cd404038a12400d687
Aaron Colbert <aaron-colbert@hotmail.com>
2018-05-10 21:23:17 -0400

    js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,29 +3,45 @@ var articleCount;
 var startYear;
 var endYear;
 
-
-$(".submit").on("click",function(){
+$(".submit").on("click", function ()
+{
     $(".submit").preventDefault();
+    AssignValues();
+
     var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     url += '?' + $.param({
-      'api-key': "3b3debd0a8764ff9b15fe098b32a079b",
-      'q': searchterm,
-      'begin_date': startYear,
-      'end_date': endYear
+        'api-key': "3b3debd0a8764ff9b15fe098b32a079b",
+        'q': searchterm,
+        'begin_date': startYear,
+        'end_date': endYear
     });
 
     $.ajax({
-      url: url,
-      method: 'GET',
-    }).done(function(result) {
-      console.log(result);
-    }).fail(function(err) {
-      throw err;
+        url: url,
+        method: 'GET',
+    }).done(function (result)
+    {
+        console.log(result);
+    }).fail(function (err)
+    {
+        throw err;
     });
 
 });
 
+function AssignValues()
+{
+    searchterm = $("#search-term").text();
+    articleCount = $("#records").text();
+    startYear = $("#start-year").text();
+    endYear = $("#end-year").text();
+    console.log(searchterm);
+    console.log(articleCount);
+    console.log(startYear);
+    console.log(endYear);
 
+
+}
 
 
 
